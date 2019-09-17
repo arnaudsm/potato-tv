b87e5526a3c501f6b2b5e0991dee2663ad56d4bb
Blantello <blantello@hotmail.fr>
2019-09-17 09:59:03 +0200

    Reamenagement back-end

--- initdb/1_TABLES.sql
+++ initdb/1_TABLES.sql
@@ -1,9 +1,22 @@
 create table users
 (
-    id bigint auto_increment,
+    user_id bigint auto_increment,
     constraint users_pk
+        primary key (user_id),
+    user_name TEXT not null,
+    password_hash TEXT not null
+);
+
+create table seen_episodes
+(
+    id bigint auto_increment,
+    constraint seen_episodes_pk
         primary key (id),
-    first_name TEXT not null,
-    last_name TEXT not null,
-    age int null
+    show_id bigint,
+    season_id int,
+    episode_id int,
+    duration_min int,
+    user_id bigint,
+    constraint seen_episodes_fk foreign key (user_id)
+    references users(user_id)
 );
